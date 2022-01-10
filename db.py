5e3dd7860f7292838feed4c3f759118ea50d0f76
yso009 <yso009@naver.com>
2022-01-10 14:49:08 +0900

    feat db.py : def db_update

--- db.py
+++ db.py
@@ -47,3 +47,21 @@ def db_insert(data):
 
     db.commit()
     db.close()
+
+def db_update(data):
+    crawl_data = data
+    db = pymysql.connect(host='localhost',
+                            port=3306,
+                            user='root',
+                            passwd='1111',
+                            db='fla',
+                            charset='utf8')
+
+    cursor = db.cursor()
+    sql = """ update fla set name = %s, nickname = %s, followers=%s, followings=%s, repository=%s, introduce=%s, search_date=%s 
+     where nickname = %s
+    """
+
+    cursor.execute(sql,(crawl_data[0], crawl_data[1], crawl_data[2], crawl_data[3], crawl_data[4], crawl_data[5], datetime.now(), crawl_data[1]))
+    db.commit()
+    db.close()
