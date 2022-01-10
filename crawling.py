7002009cb514014f73efe61cd0345fca22b384dc
yso009 <yso009@naver.com>
2022-01-10 16:33:24 +0900

    feat def get_data : try, except / add return dataType, time

--- crawling.py
+++ crawling.py
@@ -4,19 +4,24 @@ from datetime import datetime
 
 
 def get_data(id):
-    url = 'https://github.com/' + id
 
-    resp = requests.get(url)
-    soup = bs(resp.text, 'html.parser')
-    name = soup.select_one('span.p-name').get_text().replace('\n', '').replace(' ', '').lower()
-    nickname = soup.select_one('span.p-nickname').get_text().replace('\n', '').replace(' ', '').lower()
-    # email = soup.select_one('.u-email.Link--primary').attrs
-    # email = soup.find("li", itemprop="email").get_text()
-    introduce = soup.select_one('div.p-note').get_text()
-    followers = soup.select('span.text-bold')[0].get_text()
-    followings = soup.select('span.text-bold')[1].get_text()
-    repository = soup.select_one('span.Counter').get_text()
+    try:
+        url = 'https://github.com/' + id
+        
+        resp = requests.get(url)
+        soup = bs(resp.text, 'html.parser')
+        name = soup.select_one('span.p-name').get_text().replace('\n', '').replace(' ', '').lower()
+        nickname = soup.select_one('span.p-nickname').get_text().replace('\n', '').replace(' ', '').lower()
+        # email = soup.select_one('.u-email.Link--primary').attrs
+        # email = soup.find("li", itemprop="email").get_text()
+        introduce = soup.select_one('div.p-note').get_text()
+        followers = soup.select('span.text-bold')[0].get_text()
+        followings = soup.select('span.text-bold')[1].get_text()
+        repository = soup.select_one('span.Counter').get_text()
+        time = datetime.now()
 
-    return name, nickname, followers, followings, repository, introduce
+        return  name, nickname, int(followers), int(followings), int(repository), introduce, str(time)[:19]
+    except: return False
+    
 
 
