45916238e4a9918c3a879fd38735d8040aa386df
yso009 <yso009@naver.com>
2022-01-24 16:32:25 +0900

    perf crawling.py : add get contribution data

--- crawling.py
+++ crawling.py
@@ -12,16 +12,15 @@ def get_data(id):
         soup = bs(resp.text, 'html.parser')
         name = soup.select_one('span.p-name').get_text().replace('\n', '').replace(' ', '').lower()
         nickname = soup.select_one('span.p-nickname').get_text().replace('\n', '').replace(' ', '').lower()
-        # email = soup.select_one('.u-email.Link--primary').attrs
-        # email = soup.find("li", itemprop="email").get_text()
         introduce = soup.select_one('div.p-note').get_text()
         followers = soup.select('span.text-bold')[0].get_text()
         followings = soup.select('span.text-bold')[1].get_text()
         repository = soup.select_one('span.Counter').get_text()
+        contribution = soup.select('h2.text-normal')[1].get_text().replace('\n','').replace(' ', '').replace('contributionsinthelastyear', '').replace(',', '')
         time = datetime.now()
+            
 
-        return  name, nickname, int(followers), int(followings), int(repository), introduce, str(time)[:19]
+        return  name, nickname, int(followers), int(followings), int(repository), introduce, str(time)[:19], int(contribution)
+
     except: return False
     
-
-
